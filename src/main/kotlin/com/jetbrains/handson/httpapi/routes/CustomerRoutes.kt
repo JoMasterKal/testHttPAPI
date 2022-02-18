092b459fae777bf538f31044318bd1a6d75dcb3e
jonathan kalonga <jokalmwamba@gmail.com>
2022-02-18 21:55:56 +0200

    add route to get customer by his id

--- src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
@@ -8,8 +8,18 @@ fun Route.customerRouting() {
 
         }
         get("{id}") {
-
+            val id = call.parameters["id"] ?: return@get call.respondText(
+                "Missing or malformed id",
+                status = HttpStatusCode.BadRequest
+            )
+            val customer =
+                customerStorage.find { it.id == id } ?: return@get call.respondText(
+                    "No customer with id $id",
+                    status = HttpStatusCode.NotFound
+                )
+            call.respond(customer)
         }
+
         post {
 
         }
