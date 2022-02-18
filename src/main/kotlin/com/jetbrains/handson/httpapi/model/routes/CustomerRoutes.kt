1fa7d17a09c1189e0e201f4e3715b873f4e09e57
jonathan kalonga <jokalmwamba@gmail.com>
2022-02-18 21:41:24 +0200

    add routes

--- src/main/kotlin/com/jetbrains/handson/httpapi/model/routes/CustomerRoutes.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/model/routes/CustomerRoutes.kt
@@ -1,2 +1,20 @@
 package com.jetbrains.handson.httpapi.model.routes
 
+import io.ktor.routing.*
+
+fun Route.customerRouting() {
+    route("/customer") {
+        get {
+
+        }
+        get("{id}") {
+
+        }
+        post {
+
+        }
+        delete("{id}") {
+
+        }
+    }
+}
