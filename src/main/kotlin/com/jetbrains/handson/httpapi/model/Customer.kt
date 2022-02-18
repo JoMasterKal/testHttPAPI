63ce3bbf64f445325907e54a06de17bbb2d6fc7b
jonathan kalonga <jokalmwamba@gmail.com>
2022-02-18 21:39:31 +0200

    add get code to return a list of customer

--- src/main/kotlin/com/jetbrains/handson/httpapi/model/Customer.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/model/Customer.kt
@@ -1,9 +1,22 @@
 package com.jetbrains.handson.httpapi.model
 
 import kotlinx.serialization.Serializable
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+
+
+
 
 val customerStorage = mutableListOf<Customer>()
-
+        get {
+            if (customerStorage.isNotEmpty()) {
+                call.respond(customerStorage)
+            } else {
+                call.respondText("No customers found", status = HttpStatusCode.NotFound)
+            }
+        }
 
 @Serializable
 data class Customer(val id: String, val firstName: String, val lastName: String, val email: String)
