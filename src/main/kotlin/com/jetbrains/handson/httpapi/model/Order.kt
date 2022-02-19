58dddbcb49477c3e55d2a2e2cb0c6e232e5cdc96
jonathan kalonga <jokalmwamba@gmail.com>
2022-02-19 14:03:57 +0200

    add two data class order and orderItem

--- src/main/kotlin/com/jetbrains/handson/httpapi/model/Order.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/model/Order.kt
@@ -1,2 +1,9 @@
 package com.jetbrains.handson.httpapi.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Order(val number: String, val contents: List<OrderItem>)
+
+@Serializable
+data class OrderItem(val item: String, val amount: Int, val price: Double)
